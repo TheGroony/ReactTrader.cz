ea39c2b52bfe8ac21b0298001aba8ce52369767f
Jiří Strzelecki <jiri.strzelecki@gmail.com>
2021-03-15 14:07:07 +0100

    prubezny commit

--- template/js/script.js
+++ template/js/script.js
@@ -1,9 +1,18 @@
+// login page
 const inputs = document.querySelectorAll(".inputClass");
 const registerLinks = document.querySelectorAll(".register");
 const screenPrava = document.querySelector(".screen-prava");
 const screenLeva = document.querySelector(".screen-leva");
 const clouds = document.querySelector(".mobile-clouds");
 
+// Mainpage
+const openMenuBtn = document.querySelector("#openMenuBtn");
+const closeMenuBtn = document.querySelector("#closeMenuBtn");
+const overlay =  document.querySelector(".overlay");
+const blurBg = document.querySelector(".blur");
+const mainPage = document.querySelector(".mainPage");
+
+// login page input effects
 for (const input of inputs) {
     input.addEventListener("input", function() {
         if(input.value != "") {
@@ -20,4 +29,14 @@ for (const registerLink of registerLinks) {
         clouds.classList.toggle("registerCloud");
     })
 }
+
+// Mainpage burger menu
+openMenuBtn.addEventListener("click", function() {
+    overlay.style.height = "45%";
+    blurBg.style.height = "100%";
+});
+closeMenuBtn.addEventListener("click", function() {
+    overlay.style.height = "0%";
+    blurBg.style.height = "0%";
+});
 
